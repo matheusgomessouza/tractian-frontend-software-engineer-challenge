98c0992cc2d97422a5ff78fb763e216970db1125
matheus <matheusg_souza@outlook.com>
2024-06-18 10:15:58 -0300

    feat: Creating main content design / Updating README

--- README.md
+++ README.md
@@ -1,36 +1,35 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+**👨‍💻 Tecnologias / Technologies**
 
-## Getting Started
+Esse projeto foi desenvolvido com as seguintes tecnologias / This project was developed with the following technologies:
 
-First, run the development server:
+- [Next.js](https://nextjs.org/)
+- [React](https://react.dev/)
+- [Typescript](https://www.typescriptlang.org/)
+- [TailwindCSS](https://tailwindcss.com/)
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+---
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+**💻 Projeto / Project**
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+This is a Front End Software Engineer challenge from Tractian company. More information and requirements can be found in the links below:
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+GitHub: https://github.com/tractian/challenges/blob/main/front-end/README.md
 
-## Learn More
+Figma: https://www.figma.com/file/F52Yv8RmGoGOYcV9CiuIZ1/%5BCareers%5D-Frontend-Challenge-v2?type=design&node-id=0-1&mode=design&t=r3n2A4W0ZFUwVjAs-0
 
-To learn more about Next.js, take a look at the following resources:
+---
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+**🚀 Como executar / How to run**
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+- Clone o repositório / Clone the repository
+- Instale as dependências com `npm i` / Install the dependencies with `npm i`
+- Digite o comando no terminal `npm  rundev` / Type the command in the terminal `npm  run dev`
 
-## Deploy on Vercel
+---
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+**🧑🏾‍💻 Autor / Author**
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+**Matheus Gomes de Souza**
+
+LinkedIn: https://www.linkedin.com/in/matheus-gomes-de-souza/ <br/>
+E-mail: matheusg_souza@outlook.com
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 import tractianLogo from "../../public/assets/tractian-logo.svg";
 import buttonLogo from "../../public/assets/gold.svg";
-import thunderboltLogo from "../../public/assets/gold.svg";
-import exclamationLogo from "../../public/assets/gold.svg";
+import thunderboltLogo from "../../public/assets/thunderbolt.svg";
+import exclamationLogo from "../../public/assets/exclamation-circle.svg";
 
 export default function Home() {
   return (
@@ -28,22 +28,40 @@ export default function Home() {
           </button>
         </div>
       </header>
-      <main className="flex min-h-screen flex-col bg-gray-200 p-2">
-        <section className="bg-white w-full h-screen rounded-md p-4 justify-between">
-          <section className="flex gap-2">
-            <h2 className="text-black text-xl font-bold">Ativos</h2>
-            <p className="text-gray-600">/ {"Apex Unit"}</p>
-          </section>
+      <main className="flex h-screen flex-col bg-gray-200 p-2">
+        <section className="bg-white w-full h-screen rounded-md p-4">
+          <section className="flex justify-between mb-3">
+            <section className="flex gap-2">
+              <h2 className="text-black text-xl font-bold">Ativos</h2>
+              <p className="text-gray-600">/ {"Apex Unit"}</p>
+            </section>
 
-          <section className="flex justify-between">
-            <button>
-              <Image alt="" src={thunderboltLogo} />
-              Sensor de Energia
-            </button>
-            <button>
-              <Image alt="" src={exclamationLogo} />
-              Crítico
-            </button>
+            <section className="flex gap-2">
+              <button className="py-2 px-4 h-8 border border-gray-200 rounded-md flex items-center justify-center gap-2">
+                <Image alt="blue lightning symbol" src={thunderboltLogo} />
+                <p className="text-gray-600 font-bold">Sensor de Energia</p>
+              </button>
+              <button className="py-2 px-4 h-8 border border-gray-200 rounded-md flex items-center justify-center gap-2">
+                <Image alt="blue exclamation symbol" src={exclamationLogo} />
+                <p className="text-gray-600 font-bold">Crítico</p>
+              </button>
+            </section>
+          </section>
+          <section className="flex h-[96%] w-full gap-2">
+            <article className="border border-gray-200 rounded-sm w-2/5">
+              <input
+                type="search"
+                name="search"
+                id="search"
+                placeholder="Buscar Ativo ou Local"
+                className="w-full h-11 border-b border-gray-200 p-3"
+              />
+            </article>
+            <aside className="border border-gray-200 rounded-sm w-3/5">
+              <header className="border-b border-gray-200 h-14 flex items-center px-4">
+                <strong className="text-black">{"MOTOR RT COAL AF01"}</strong>
+              </header>
+            </aside>
           </section>
         </section>
       </main>
